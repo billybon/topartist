21c06e25dd0339332ce0635f0dc48a8e022f549c
billybon <bilevich7980400>
2012-12-16 18:01:56 +0300

    Добавил проект

---
+++ src/ru/topartist/action/HomeAction.java
@@ -0,0 +1,17 @@
+package ru.topartist.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alexey
+ * Date: 12.12.12
+ * Time: 13:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class HomeAction extends ActionSupport {
+    public String execute() {
+
+        return "SUCCESS";
+    }
+}
---
+++ src/ru/topartist/action/Page1Action.java
@@ -0,0 +1,17 @@
+package ru.topartist.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alexey
+ * Date: 12.12.12
+ * Time: 14:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class Page1Action extends ActionSupport {
+    public String execute() {
+
+        return "SUCCESS";
+    }
+}
